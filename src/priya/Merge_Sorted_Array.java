380ff0c0e975b417497858d3448e5a0f392e79ff
roulpriyambada09 <roulpriyambada@gmail.com>
2021-09-17 12:44:23 +0530

    merge sorted array

---
+++ src/priya/Merge_Sorted_Array.java
@@ -0,0 +1,20 @@
+package priya;
+
+import java.util.Arrays;
+
+public class Merge_Sorted_Array {
+	
+	    public void merge(int[] nums1, int m, int[] nums2, int n) {
+	        
+	        int l=m+n;
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            nums1[i+m] = nums2[i];
+	        }
+
+	        Arrays.sort(nums1);
+	        System.out.println(Arrays.toString(nums2));
+	    }
+	}
+
